1071bde3118056bfca2bb120cb676b9b49d9f410
Solsol29 <Adrianix.2007.73SM@gmail.com>
2026-06-05 09:56:10 -0500

    version2

--- proyectofinal/index.php
+++ proyectofinal/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Solicitud de matrícula</title>
+    <title>Solicitud de matrícula2026</title>
     <link rel="stylesheet" href="view.css">
 </head>
 <body>
